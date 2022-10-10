bc0ccfdf1942cf8baf606b051c2bc4bbd1739178
Teviets <est21405@uvg.edu.gt>
2022-10-10 10:36:29 -0600

    x

--- Jugador.cpp
+++ Jugador.cpp
@@ -1,29 +1,32 @@
 #include <string.h>
-#include <stdlib.h>
-#include <iostream>
 
 class Jugador{
-    private:
-        string nombre();
-        int salario;
-        int valor_de_mercado;
-    
-    public:
-        Jugador(string _nombre,int _salario,int _valor_de_mercado){
-		    nombre = _nombre;
-		    salario = _salario;
-		    valor_de_mercado = _valor_de_mercado;
-		}	            
-        
-        string getNombre(){
-			return nombre;
-		}
-        int getSalario(){
-			return salario;
-		}
-        int getValorMercado(){
-			return valor_de_mercado
-		}
+	private:
+		string nombre;
+		int salario;
+		int valor_de_mercado;
+	
+	public:
+		Jugador(int _salario,int _valor_de_mercado, string nombre);
+		string getNombre();
+		int getSalario();
+		int getValorDeMercado();
 };
 
+Jugador::	Jugador(int _salario, int _valor_de_mercado, string _nombre){
+	nombre = _nombre;
+	salario = _salario;
+	valor_de_mercado = _valor_de_mercado;
+}
 
+Jugador::getNombre(){
+	return nombre;
+}
+
+Jugador::getSalario(){
+	return salario;
+}
+
+Jugador::getValorDeMercado(){
+	return valor_de_mercado
+}
